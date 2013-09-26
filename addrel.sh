77de1d7efd0b8d2dc2add2c6be7024fa9905dcef
inox9 <inox@x7.com>
2013-09-26 12:54:12 +0300

    addrel.sh fix

--- addrel.sh
+++ addrel.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+DBPATH = /home/pi/mbot/scenerel.db
+
 if [ $# -ne 1 ]; then
 	echo 'wrong argument count'
 	exit
 fi
 
-sqlite3 /home/pi/mbot/scenerel.db "insert into awaiting(dateadded,keywords,state) values(strftime('%s','now'),'$1',0)"
+sqlite3 $DBPATH "insert into awaiting(dateadded,keywords,state) values(strftime('%s','now'),'$1',0)"
