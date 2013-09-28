5e96393fe87f02fc89646d75ac7db472b22444b2
inox9 <inox@x7.com>
2013-09-28 14:45:06 +0300

    добавлен простейший веб-интерфейс для добавления и просмотра статусов

---
+++ www/api.php
@@ -0,0 +1,51 @@
+<?php
+
+$config = '/home/pi/mbot/mbot.ini';
+
+$conf = @parse_ini_file($config, true);
+try {
+	$db = new PDO('sqlite:' . $conf['main']['db']);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	die($e->getMessage());
+}
+
+switch ($_REQUEST['action']) {
+	case 'add':
+	$qr = 'insert into awaiting (dateadded,keywords,state) values (?, ?, 0)';
+	$res = $db->prepare($qr);
+	if ($res->execute(array(time(), $_POST['keywords']))) print 'OK';
+	else die('DB error!');
+	break;
+
+	case 'get':
+	if (!$res = $db->query('select id,dateadded,keywords,releasename,releasedate,releasepage,state from awaiting order by dateadded desc')) {
+		die('DB error!');
+	}
+	$result = array();
+	while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+		$row['dateAdded'] = date('d.m.Y H:i', $row['dateAdded']);
+		if (!empty($row['releaseDate'])) {
+			$row['releaseDate'] = date('d.m.Y H:i', $row['releaseDate']);
+		}
+
+		if (!empty($row['releasePage']) && $row['state'] == 1) {
+			$row['releaseName'] = sprintf('<a href="%s">%s</a>', $row['releasePage'], $row['releaseName']);
+		}
+
+		if ($row['state'] == 2) {
+			$row['releaseName'] = sprintf('<a href="%s">%s</a>', $conf['main']['downWebPath'] . '/' . $row['releaseName'] . '/', $row['releaseName']);
+		}		
+
+		if ($row['state'] == 0) {
+			$row['keywords'] = sprintf('<a href="%s">%s</a>', 'http://predb.me/?cats=music&search=' . $row['keywords'], $row['keywords']);
+		}
+
+		$result[] = $row;
+	}
+	header('Content-type: application/json');
+	print json_encode($result);
+	break;
+}
+
+$db = null;
