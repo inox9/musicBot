12af379553ae02d96cab771807539485e932b732
inox9 <inox@x7.com>
2013-10-27 12:09:44 +0200

    api small year detection fix

--- www/api.php
+++ www/api.php
@@ -12,43 +12,44 @@ try {
 
 switch ($_REQUEST['action']) {
 	case 'add':
-	$res = $db->prepare('insert into awaiting (dateadded,keywords,state) values (?, ?, 0)');
-	try {
-		$res->execute(array(time(), $_POST['keywords']));
-	} catch (PDOException $e) {
-		die($e->getMessage());
-	}
-	print 'OK';
-	break;
+		$res = $db->prepare('insert into awaiting (dateadded,keywords,state) values (?, ?, 0)');
+		try {
+			$res->execute(array(time(), $_POST['keywords']));
+		} catch (PDOException $e) {
+			die($e->getMessage());
+		}
+		print 'OK';
+		break;
 
 	case 'get':
-	if (!$res = $db->query('select dateadded,keywords,releasename,releasedate,releasepage,state from awaiting order by state,-releasedate')) {
-		die('DB error!');
-	}
-	$result = array();
-	while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
-		$row['dateAdded'] = date('d.m.Y H:i', $row['dateAdded']);
-		if (!empty($row['releaseDate'])) {
-			$row['releaseDate'] = date('d.m.Y H:i', $row['releaseDate']);
+		if (!$res = $db->query('select dateadded,keywords,releasename,releasedate,releasepage,state from awaiting order by state,-releasedate')) {
+			die('DB error!');
 		}
+		$result = array();
+		while ($row = $res->fetch(PDO::FETCH_ASSOC)) {
+			$row['dateAdded'] = date('d.m.Y H:i', $row['dateAdded']);
+			if (!empty($row['releaseDate'])) {
+				$row['releaseDate'] = date('d.m.Y H:i', $row['releaseDate']);
+			}
 
-		if (!empty($row['releasePage']) && $row['state'] == 1) {
-			$row['releaseName'] = sprintf('<a href="%s">%s</a>', $row['releasePage'], $row['releaseName']);
+			if (!empty($row['releasePage']) && $row['state'] == 1) {
+				$row['releaseName'] = sprintf('<a href="%s">%s</a>', $row['releasePage'], $row['releaseName']);
+			}
+
+			if ($row['state'] == 2) {
+				preg_match('/(\d{4})-\w+$/', $row['releaseName'], $rem);
+				$row['releaseName'] = sprintf('<a href="%s">%s</a>', $conf['main']['downWebPath'] . '/' . $rem[1] . '/' . $row['releaseName'] . '/', $row['releaseName']);
+			}		
+
+			if ($row['state'] == 0) {
+				$row['keywords'] = sprintf('<a href="%s">%s</a>', 'http://predb.me/?cats=music&search=' . $row['keywords'], $row['keywords']);
+			}
+
+			$result[] = $row;
 		}
-
-		if ($row['state'] == 2) {
-			$row['releaseName'] = sprintf('<a href="%s">%s</a>', $conf['main']['downWebPath'] . '/' . $row['releaseName'] . '/', $row['releaseName']);
-		}		
-
-		if ($row['state'] == 0) {
-			$row['keywords'] = sprintf('<a href="%s">%s</a>', 'http://predb.me/?cats=music&search=' . $row['keywords'], $row['keywords']);
-		}
-
-		$result[] = $row;
-	}
-	header('Content-type: application/json');
-	print json_encode($result);
-	break;
+		header('Content-type: application/json');
+		print json_encode($result);
+		break;
 }
 
 $db = null;
