4d8a5e768f9e6def3a073f4d043d5956b7fc4a6c
inox <inox@corp>
2014-07-14 13:45:43 +0300

    removed messi css/js, moved angular js to separate file

---
+++ www/media/app.js
@@ -0,0 +1,72 @@
+var app = angular.module('App', []);
+app.controller('releasesCtrl', function releasesCtrl($scope, $http) {
+	$scope.formData = {};
+
+	$scope.fetchList = function() {
+		$http.get('/api.php?action=get')
+			.success(function(data) {
+				$scope.releases = data;
+			})
+			.error(function(data) {
+				alert('Error occurred: ' + data);
+			});
+	}
+
+	$scope.rowClass = function(rel) {
+		switch (rel.state) {
+			case '1': return 'warning';
+			case '2': return 'success';
+			default: return '';
+		}
+	}
+
+	$scope.processField = function(val) {
+		return val ? val : 'N/A';
+	}
+
+	$scope.processName = function(rel) {
+		if (rel.releaseUrl) {
+			return rel.releaseUrl;
+		} else if (rel.releasePage) {
+			return rel.releasePage;
+		} else {
+			return null;
+		}
+	}
+
+	$scope.add = function() {
+		$scope.formData['action'] = 'add';
+		$http({
+			method: 'POST', 
+			url: '/api.php', 
+			data: $.param($scope.formData), 
+			headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+		}).success(function(data) {
+			if (data == 'OK') {
+				alert('Релиз добавлен успешно!');
+				$scope.fetchList();
+			} else {
+				alert('Error occured: ' + data);
+			}
+		}).error(function(data) {
+			alert('Error occured: ' + data);
+		});
+	}
+
+	$scope.remove = function(rid) {
+		if (confirm('Хотите удалить этот релиз из очереди ожидания?')) {
+			$http.get('/api.php?action=remove&id=' + rid)
+			.success(function(data) {
+				if (data == 'OK') {
+					alert('Релиз удален успешно!');
+					$scope.fetchList();
+				}
+			})
+			.error(function(data) {
+				alert('Error occurred: ' + data);
+			});
+		}
+	}
+
+	$scope.fetchList();
+});
