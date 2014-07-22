60315ad8c7e7a12f74c7fd72769d36a93214da3e
djinox <inox@li.ru>
2014-07-22 23:56:46 +0300

    fixed leecherus module to reflect recent changes in leecher.us site

--- modules/leecherus.py
+++ modules/leecherus.py
@@ -4,6 +4,7 @@
 	version 0.1
 '''
 
+import re
 from selenium.webdriver import PhantomJS
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -19,13 +20,14 @@ class Leecherus(object):
 		try:
 			self.browser.get('http://leecher.us')
 			wdw = WebDriverWait(self.browser, 10)
-			wdw.until(EC.visibility_of_element_located((By.NAME, 'link'))).send_keys(self.url)
+			wdw.until(EC.visibility_of_element_located((By.NAME, 'links'))).send_keys(self.url)
 			wdw.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="subscribe"]'))).click()
-			wdw.until(EC.element_to_be_clickable((By.ID, 'get_link'))).click()
+			wdw.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="subscribe"]'))).click()
 			self.browser.switch_to_window(self.browser.window_handles[1])
-			url = wdw.until(EC.presence_of_element_located((By.XPATH, '//a[@title="click here to download"]'))).get_attribute('href')
+			onclick = wdw.until(EC.element_to_be_clickable((By.XPATH, '//button[@class="subscribe"]'))).get_attribute('onclick')
 		except (WebDriverException, NoSuchElementException, TimeoutException):
 			return False
 		finally:
 			self.browser.quit()
-		return url
+		m = re.search("'(http://[^']+)'", onclick)
+		return m.group(1) if m else False
